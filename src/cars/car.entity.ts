46d605ab52c0424fc5790063433d2658b4054a17
Luiz Gustavo Sampaio Mafra de Santana <lgmafra@gmail.com>
2019-05-23 21:59:15 -0300

    included plate field

--- src/cars/car.entity.ts
+++ src/cars/car.entity.ts
@@ -1,19 +1,22 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Car{
-    @PrimaryGeneratedColumn()
-    id: number
+export class Car {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({length: 50})
-    maker: string
+  @Column({ length: 50 })
+  maker: string;
 
-    @Column({length: 50})
-    model: string
+  @Column({ length: 50 })
+  model: string;
 
-    @Column({length: 20})
-    color: string
+  @Column({ length: 20 })
+  color: string;
 
-    @Column({length: 3})
-    engine: string
+  @Column({ length: 3 })
+  engine: string;
+
+  @Column({ length: 7, nullable: true })
+  plate: string;
 }
